c2097ff46575f55f59d99f74a9cf9d9b5b1c3a73
Harivansh Bhardwaj <harivanshbhardwaj57@gmail.com>
2024-05-27 12:50:33 +0530

    Create 04operations.js

---
+++ Java Script/04operations.js
@@ -0,0 +1,39 @@
+let value=3
+let negValue=-value
+console.log(negValue)
+
+
+console.log(2+2);
+console.log(2-2);
+console.log(2/2);
+console.log(2*2);
+console.log(2**3);
+console.log(2%2);
+
+console.log("Hello")
+let str1="Hello";
+let str2=" Harivansh"
+let str3=str1+str2;
+console.log(str3);
+
+
+console.log("1"+2)  //12
+console.log(1+"2");  //12
+console.log("1"+2+2);  //122
+console.log(1+2+"2");  //32
+
+console.log(+true)
+console.log(+false)
+console.log(+"")
+
+
+
+let num1,num2,num3;
+num1=num2=num3=2+2
+
+let gamecounter =100;
+++gamecounter;
+console.log(gamecounter)
+
+//prefix and postfix 
+
