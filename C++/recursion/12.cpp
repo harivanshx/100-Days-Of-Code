ca92409d3355cef12714560d71c3391127ec74b4
Harivansh Bhardwaj <harivanshbhardwaj57@gmail.com>
2024-06-28 23:18:14 +0530

    recursion Fibonacci

---
+++ C++/recursion/12.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int fibo(int n){
+
+    if(n<=1)return n;
+
+    return (fibo(n-1)+fibo(n-2));
+
+}
+
+
+int main()
+{
+
+    int k=6;
+    cout<<fibo(k);
+
+return 0;
+}
