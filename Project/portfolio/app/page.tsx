3f40d93fc245d3cea6844cbf9f93959017a36fa3
Harivansh Bhardwaj <harivanshbhardwaj57@gmail.com>
2024-07-14 23:18:28 +0530

    portfolio porject

---
+++ Project/portfolio/app/page.tsx
@@ -0,0 +1,13 @@
+import Hero from "@/components/Hero";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10">
+      <div className="max-w-7xl w-full">
+       
+        <Hero/>
+      </div>
+    </main>
+  );
+}
