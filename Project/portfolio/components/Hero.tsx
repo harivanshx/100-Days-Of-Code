86a55989587db0673afd5669a2fa37758448f22c
Harivansh Bhardwaj <harivanshbhardwaj57@gmail.com>
2024-07-20 23:19:25 +0530

    code push

--- Project/portfolio/components/Hero.tsx
+++ Project/portfolio/components/Hero.tsx
@@ -9,6 +9,14 @@ const Hero = () => {
      <Spotlight className='-top-10 -left-full h-[80vh] w-[50vh]' fill='purple'/>
        <Spotlight className='top-28 left-80 h-[80vh] w-[50vw]
        ' fill='blue'/>
+
+<div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.3] bg-grid-black/[0.2] relative flex items-center justify-center">
+      {/* Radial gradient for the container to give a faded look */}
+      <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+     
+        Backgrounds
+     
+    </div>
     </div>
     </div>
 
