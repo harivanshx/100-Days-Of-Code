c5ce21e27b5b1ff35430f1e4d4dc4f32a8b4d682
Harivansh Bhardwaj <harivanshbhardwaj57@gmail.com>
2024-07-23 23:27:02 +0530

    CodePush

---
+++ Java Script/08String.js
@@ -0,0 +1,24 @@
+//Modern String Writing
+// "Hello"
+// 'Hello'  both are same
+
+const name = "Harivansh ";
+const repoCount = 50;
+ 
+// console.log(name + repoCount + " value");
+// dont use this syntex as it is outdatted
+
+
+// in modern date we will use backtics``
+
+console.log(   `Hello my name is ${name} abd my repo count is ${repoCount}`);
+
+
+const gameName = new String('Harivansh')
+console.log(gameName);
+console.log(gameName.length);
+console.log(gameName.__proto__);
+console.log(gameName.toLowerCase());
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt('2'));
+console.log(gameName.indexOf('r'));
